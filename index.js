5c98a8e60252e3b5897a5871a69ccf8b01ccf503
MohamedAmineXCVIII <mohamedamine.dammak98@gmail.com>
2023-12-06 14:39:14 +0100

    connecting the backend to a database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note");
 
 const app = express();
 
@@ -39,17 +41,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((n) => n.id === id);
-  if (note) {
+  Note.findById(req.params.id).then((note) => {
     res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 const generateId = () => {
@@ -66,13 +66,14 @@ app.post("/api/notes", (req, res) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
-  notes = [...notes, note];
-  res.json(note);
+  });
+
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (req, res) => {
@@ -87,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
 });
---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://mohamedaminedammak98:${password}@cluster0.vgykwlt.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// I- code for generating new notes and saving them to the db
+// const note = new Note({
+//   content: "GET and POST are the most important methods of HTTP protocol",
+//   important: true,
+// });
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+// II- code for Fetching objects from the database
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
